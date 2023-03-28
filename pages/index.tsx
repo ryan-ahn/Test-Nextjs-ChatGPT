7ff0940be69130693cc427072d055d2d83222203
Ahn-Ryan <kaaiinn4@gmail.com>
2023-03-28 15:02:30 +0900

    Modify: clear

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'styles/global';
 import theme from '@styles/theme';
-import Main from '@components/main';
+import Canvas from '@components/2. canvas';
 
 export default function Home() {
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
-        <Main />
+        <Canvas />
       </ThemeProvider>
     </>
   );
